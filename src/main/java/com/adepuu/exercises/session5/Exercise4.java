7adb8888b5adc4129d362ebf3e26fd9a9bae4528
gerry-sheva <gerry.sheva@proton.me>
2024-03-25 12:17:46 +0700

    exercise5 blocked

--- src/main/java/com/adepuu/exercises/session5/Exercise4.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise4.java
@@ -3,13 +3,6 @@ package com.adepuu.exercises.session5;
 import java.util.ArrayList;
 
 public class Exercise4 {
-    /**
-     * Java Array Program to Remove Duplicate Elements From an Array
-     *
-     * Input: [ 1, 2, 2, 3, 3, 3, 4, 5 ]
-     * Output: [ 1, 2, 3, 4, 5 ]
-     * @param args
-     */
     public static void main(String[] args) {
         Solution4 solution4 = new Solution4();
         solution4.removeAllOccurrences();
--- src/main/java/com/adepuu/exercises/session5/Exercise5.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise5.java
@@ -1,18 +1,57 @@
 package com.adepuu.exercises.session5;
 
-public class Exercise5 {
-    /**
-     * Java Array Program to sort array in increasing & decreasing order
-     *
-     * Example 1:
-     * Input: [ 8, 7, 5, 2], direction = “asc”
-     * Output: [ 2, 5, 7, 8 ]
-     *
-     * Example 2:
-     * Input: [ 8, 7, 5, 2], direction = “desc”
-     * Output: [ 8,7, 5, 2 ]
-     */
-    public static void main(String[] args) {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Exercise5 {
+    public static void main(String[] args) {
+        Solution5 solution5 = new Solution5();
+        boolean result = solution5.anagram();
+        System.out.println(result);
+    }
+}
+
+class Solution5 {
+    public boolean anagram() {
+        String input = "anagram";
+        String target = "nagaram";
+
+        HashMap<Character, Integer> inputHash = new HashMap<Character, Integer>();
+        for (int i = 0; i < input.length(); i++) {
+            if (inputHash.containsKey(input.charAt(i))){
+                inputHash.put(input.charAt(i), 1);
+            } else {
+                Integer freq = inputHash.get(input.charAt(i)) + 1;
+                inputHash.put(input.charAt(i), freq);
+            }
+        }
+
+        HashMap<Character, Integer> targetHash = new HashMap<Character, Integer>();
+        for (int i = 0; i < target.length(); i++) {
+            if (!inputHash.containsKey(target.charAt(i))){
+                return false;
+            }
+            if (targetHash.containsKey(target.charAt(i))){
+                targetHash.put(target.charAt(i), 1);
+            } else {
+                int freq = targetHash.get(target.charAt(i)) + 1;
+                targetHash.put(target.charAt(i), freq);
+            }
+        }
+
+        if (inputHash.size() != targetHash.size()) {
+            return false;
+        }
+
+        for (Map.Entry<Character, Integer> entry: inputHash.entrySet()) {
+            Character key = entry.getKey();
+            Integer value = entry.getValue();
+            Integer targetValue = inputHash.get(key);
+
+            if (!value.equals(targetValue)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
