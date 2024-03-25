1db01a7e354e1bb5b5e9914604c182197137302f
gerry-sheva <gerry.sheva@proton.me>
2024-03-25 11:09:55 +0700

    solved exercise2

--- src/main/java/com/adepuu/exercises/session5/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise2.java
@@ -1,14 +1,22 @@
 package com.adepuu.exercises.session5;
 
 public class Exercise2 {
-    /**
-     * Java Array Program For Array Rotation
-     * Input: arr[] = {1, 2, 3, 4, 5, 6, 7},  d = 2
-     * Output:  3 4 5 6 7 1 2
-     * Explanation: d=2 so 2 elements are rotated to the end of the array. So, 1 2 is rotated back
-     * So, Final result: 3, 4, 5, 6, 7, 1, 2
-     */
     public static void main(String[] args) {
+        Solution2 solution2 = new Solution2();
+        boolean result = solution2.findDuplicate();
+        System.out.println(result);
+    }
+}
 
+class Solution2 {
+    public boolean findDuplicate() {
+        int[] input = {1,2,3,4};
+        Quicksort.quickSort(input, 0, input.length - 1);
+        for (int i = 1; i < input.length; i++) {
+            if (input[i] == input[i - 1]) {
+                return true;
+            }
+        }
+        return false;
     }
 }
