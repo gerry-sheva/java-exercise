b2398c9b9ee9baa55efe586161e229bb52b052cb
gerry-sheva <gerry.sheva@proton.me>
2024-03-26 13:37:54 +0700

    solved exercise2

--- src/main/java/com/adepuu/exercises/session6/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise2.java
@@ -1,7 +1,6 @@
 package com.adepuu.exercises.session6;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Scanner;
 
 public class Exercise2 {
@@ -10,19 +9,44 @@ public class Exercise2 {
      * and handle all edge cases using try-catch-finally based on the logic given
      */
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Enter the file name: ");
-//        String fileName = scanner.nextLine();
-//        readFileContents(fileName);
-//        scanner.close();
+        Scanner scanner = new Scanner(System.in);
+        boolean isDone = false;
+
+        try {
+            System.out.print("Enter the file name: ");
+            String fileName = scanner.nextLine();
+            readFileContents(fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("File is not found");
+        } catch (EOFException e) {
+            System.out.println("EOF");
+        } catch (IOException e) {
+            System.out.println("Something went wrong during reading the file");
+        } catch (Exception e) {
+            System.out.println("System error! Please try again later");
+        }
+        finally {
+            scanner.close();
+        }
+
     }
 
-    static void readFileContents(String fileName) {
-//        BufferedReader reader = null;
-//        reader = new BufferedReader(new FileReader(fileName));
-//        String line;
-//        while ((line = reader.readLine()) != null) {
-//            System.out.println(line);
-//        }
+    static void readFileContents(String fileName) throws Exception {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            if (e.getClass().equals(FileNotFoundException.class)) {
+                throw new FileNotFoundException();
+            } else if (e.getClass().equals(EOFException.class)) {
+                throw new EOFException();
+            } else {
+                throw new IOException();
+            }
+        } catch (Exception e) {
+            throw new Exception();
+        }
     }
 }
