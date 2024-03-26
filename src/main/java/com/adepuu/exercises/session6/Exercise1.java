f683a2f2f8cf790d79cfb2933b82f7e0b9e6c223
gerry-sheva <gerry.sheva@proton.me>
2024-03-26 11:47:14 +0700

    solved exercise1

--- src/main/java/com/adepuu/exercises/session6/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise1.java
@@ -1,5 +1,8 @@
 package com.adepuu.exercises.session6;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Exercise1 {
     /**
      * Write a Java Program to Enter numbers to calculate average and enter 'q' to finish.
@@ -13,6 +16,34 @@ public class Exercise1 {
      * Explanation: print “Invalid input. Please enter a valid number or 'q' to finish.” if user not inserting the expected number or character.
      */
     public static void main(String[] args) {
+//        System.out.println("wha");
+        Solution solution = new Solution();
+        solution.calculateAvg();
+    }
+}
 
+class Solution {
+
+    public void calculateAvg() {
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0;
+        double i = 0;
+        boolean isDone = false;
+        while (!isDone) {
+            String input = scanner.next();
+            if (input.equals("q") || input.equals("Q")) {
+                isDone = true;
+            } else {
+                try {
+                    sum += Double.parseDouble(input);
+                    ++i;
+                } catch (Exception e) {
+                    System.out.println("Invalid input. Please enter a valid number or 'q' to finish.");
+                    System.out.printf("The last average of your inputs is: %f", sum/i);
+                    isDone = true;
+                }
+            }
+        }
+        System.out.println(sum/i);
     }
 }
--- src/main/java/com/adepuu/exercises/session6/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise2.java
@@ -2,6 +2,7 @@ package com.adepuu.exercises.session6;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class Exercise2 {
     /**
@@ -9,19 +10,19 @@ public class Exercise2 {
      * and handle all edge cases using try-catch-finally based on the logic given
      */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the file name: ");
-        String fileName = scanner.nextLine();
-        readFileContents(fileName);
-        scanner.close();
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter the file name: ");
+//        String fileName = scanner.nextLine();
+//        readFileContents(fileName);
+//        scanner.close();
     }
 
     static void readFileContents(String fileName) {
-        BufferedReader reader = null;
-        reader = new BufferedReader(new FileReader(fileName));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
-        }
+//        BufferedReader reader = null;
+//        reader = new BufferedReader(new FileReader(fileName));
+//        String line;
+//        while ((line = reader.readLine()) != null) {
+//            System.out.println(line);
+//        }
     }
 }
